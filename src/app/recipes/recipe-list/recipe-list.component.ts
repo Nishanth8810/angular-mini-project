21ed6908fbd24409e5d185fc010006c262966692
Nishanth <nishanths8810@gmail.com>
2024-01-05 20:34:44 +0530

    completed initial work

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
-
+export class RecipeListComponent implements OnInit{
+  Recipe:Recipe[]=[
+    new Recipe('A Test Recipe','Really good recipe','https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1704412800&semt=sph.google.com/url?sa=i&url=https%3A%2F%2Fwww.foodandwine.com%2Frecipes%2Fbucatini-with-mushroom-ragu-dandelion-greens-and-tarragon&psig=AOvVaw3qGGEB_vsKU3iWO9M3kJLE&ust=1704544683337000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOj0ovahxoMDFQAAAAAdAAAAABAE')
+  ,new Recipe('A Test Recipe 2','Really good recipe 2','https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1704412800&semt=sph.google.com/url?sa=i&url=https%3A%2F%2Fwww.foodandwine.com%2Frecipes%2Fbucatini-with-mushroom-ragu-dandelion-greens-and-tarragon&psig=AOvVaw3qGGEB_vsKU3iWO9M3kJLE&ust=1704544683337000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOj0ovahxoMDFQAAAAAdAAAAABAE')
+];
+  
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+
+  Ingredient:Ingredient[]=[new Ingredient('tomato',12),new Ingredient('apple',10)];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
